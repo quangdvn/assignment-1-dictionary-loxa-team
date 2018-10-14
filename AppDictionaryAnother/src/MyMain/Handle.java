af893a24e38f81e2acc615c21c55ea8decfd0f38
NgocHai220998 <41608661+NgocHai220998@users.noreply.github.com>
2018-10-14 22:34:29 +0700

    2nd Commit

--- AppDictionaryAnother/src/MyMain/Handle.java
+++ AppDictionaryAnother/src/MyMain/Handle.java
@@ -1,92 +1,148 @@
 package MyMain;
 
 import Database.Controller;
+import MyFX.AlertBoxAdd;
+import MyFX.AlertBoxTranslate;
 import MyFX.CButton;
 import MyFX.CListView;
 import MyFX.CTextField;
 import MyFX.CWebView;
+import MyFX.ListWord;
 import MyScene.primaryCScene;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.InputEvent;
 
 public class Handle {
+
 	public static Scene primaryScene() {
+		
 		primaryCScene.Init();
+			// Init
+			CButton button_translate = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_add = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_game = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_info = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_quit = new CButton(primaryCScene.getRoot(),"button-quit");
+			CButton button_search = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_sound = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_restore = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_like = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_remove = new CButton(primaryCScene.getRoot(),"button");
+			CButton button_login = new CButton(primaryCScene.getRoot(),"button");
+					
+			ObservableList<String> list = FXCollections.observableArrayList(
+	                "Dictionary", "Like List", "List Delete", "History");
+			ChoiceBox<String> choiceBox = new ChoiceBox<>();
+			 
 
-		// Google translate button
-		CButton button_translate = new CButton(primaryCScene.getRoot(),"button");
-		button_translate.Init(14.0, 59.0, 122.0, 88.0, "Translate");
+	        choiceBox.setPrefSize(179.0, 37.0);
+	        choiceBox.setLayoutX(146.0);          choiceBox.setLayoutY(14.0);
+	        choiceBox.setItems(list);
+	        choiceBox.getSelectionModel().selectFirst();
+	 
+	        choiceBox.getSelectionModel().selectedIndexProperty()
+	                .addListener(new ChangeListener<Number>() {
+	                    public void changed(ObservableValue ov, Number value, Number new_value) {
+	                    	System.out.println(list.get(new_value.intValue()).toUpperCase());
+	                    }
+	                });
+	        
+			primaryCScene.getRoot().getChildren().addAll(choiceBox);
+			
+			CWebView web_view = new CWebView(primaryCScene.getRoot(),"web-view");
+			CListView list_view = new CListView(primaryCScene.getRoot(),"list-view");
+			CTextField text_filde_search = new CTextField(primaryCScene.getRoot(),"text-filde-search");
+			// Google translate button
+			button_translate.Init(14.0, 14.0, 122.0, 73.0, "Translate");
+			button_translate.getButton().setOnAction(e -> AlertBoxTranslate.display("Google Translate"));
+			// add button 
+			button_add.Init(608.0, 54.0, 108.0, 37.0, "Add");
+			button_add.getButton().setOnAction(new EventHandler<ActionEvent>() {
+			       @Override
+			       public void handle(ActionEvent event) {
+			    	   AlertBoxAdd.display("Add new word");
+			    	   //Controller.deleteWord(list_view.getListView(), web_view.getWebView());
+			           ListWord.setListTarget(Controller.loadDataBase());
+			           list_view.showListView(text_filde_search.getTextField().getText());
+			       }
+			});
+			
+			
+			
+			// Game button
+			button_game.Init(14.0, 108.0, 122.0, 73.0, "Game");
+			// Info button
+			button_info.Init(14.0, 196.0, 122.0, 73.0, "Info");
+			// Quit button
+			button_quit.Init(14.0, 369.0, 122.0, 73.0, "Quit");
+			button_quit.getButton().setOnAction(new EventHandler<ActionEvent>() {
+			       @Override
+			       public void handle(ActionEvent event) {
+			    	   Platform.exit();
+	    		       System.exit(0);
+			       }
+			});
+			// Button search
+			button_search.Init(608.0, 17.0, 108.0, 31.0, "Search");
+			button_search.getButton().setOnAction(new EventHandler<ActionEvent>() {
+			       @Override
+			       public void handle(ActionEvent event) {
+			    	   Controller.displaySelectedItem(list_view.getListView(), web_view.getWebView(),text_filde_search.getTextField().getText());
+			       }
+			});
+			// Button sound
+			button_sound.Init(332.0, 57.0, 81.0, 31.0, "Sound");
+			// Restore Button
+			button_restore.Init(518.0, 57.0, 81.0, 31.0, "Restore");
+			// Like Button
+			button_like.Init(662.0, 99.0, 52.0, 31.0, "Like");
+			// Remove button
+			button_remove.Init(425.0, 57.0, 81.0, 31.0, "Remove");
+			/**
+			 * remove word and update database
+			 */
+			button_remove.getButton().setOnAction(new EventHandler<ActionEvent>() {
+			       @Override
+			       /**
+			        * hadle data
+			        */
+			       public void handle(ActionEvent event) {
+			              Controller.deleteWord(list_view.getListView(), web_view.getWebView());
+			              ListWord.setListTarget(Controller.loadDataBase());
+			              list_view.showListView(text_filde_search.getTextField().getText());
+			       }
+			});
+			// Login button
+			button_login.Init(14.0, 281.0, 122.0, 73.0, "Login");
 
-		// Game button
-		CButton button_game = new CButton(primaryCScene.getRoot(),"button");
-		button_game.Init(14.0, 160.0, 122.0, 88.0, "Game");
-		// Info button
-		CButton button_info = new CButton(primaryCScene.getRoot(),"button");
-		button_info.Init(14.0, 254.0, 122.0, 88.0, "Info");
-		// Quit button
-		CButton button_quit = new CButton(primaryCScene.getRoot(),"button-quit");
-		button_quit.Init(602.0, 353.0, 114.0, 88.0, "Quit");
-		// Button search
-		CButton button_search = new CButton(primaryCScene.getRoot(),"button");
-		button_search.Init(602.0, 59.0, 114.0, 31.0, "Search");
-		
-		// Button sound
-		CButton button_sound = new CButton(primaryCScene.getRoot(),"button");
-		button_sound.Init(602.0, 95.0, 114.0, 59.0, "Sound");
-		// Restore Button
-		CButton button_restore = new CButton(primaryCScene.getRoot(),"button");
-		button_restore.Init(602.0, 289.0, 114.0, 59.0, "Restore");
-		// Like Button
-		CButton button_like = new CButton(primaryCScene.getRoot(),"button");
-		button_like.Init(602.0, 160.0, 114.0, 59.0, "Like");
-		// Remove button
-		CButton button_remove = new CButton(primaryCScene.getRoot(),"button");
-		button_remove.Init(602.0, 224.0, 114.0, 59.0, "Remove");
-		// Login button
-		CButton button_login = new CButton(primaryCScene.getRoot(),"button");
-		button_login.Init(14.0, 353.0, 122.0, 88.0, "Login");
-		// Like list Button
-		CButton button_like_list = new CButton(primaryCScene.getRoot(),"button");
-		button_like_list.Init(146.0, 14.0, 179.0, 37.0, "Like List");
-		// History button
-		CButton button_history = new CButton(primaryCScene.getRoot(),"button");
-		button_history.Init(332.0, 14.0, 133.0, 37.0, "History");
-		// Delelist Button
-		CButton button_delete_list = new CButton(primaryCScene.getRoot(),"button");
-		button_delete_list.Init(470.0, 14.0, 133.0, 37.0, "Delete List");
-		
-  
-		//
-		
-/*		// TextArea
-		CTextArea text_area = new CTextArea(primaryCScene.getRoot(),"text-area");
-		text_area.Init(332.0, 95.0, 267.0, 347.0);*/
-		
-		CWebView web_view = new CWebView(primaryCScene.getRoot(),"web-view");
-		web_view.init(332.0, 95.0, 267.0, 347.0);
-		
-		// ListView
-		CListView list_view = new CListView(primaryCScene.getRoot(),"list-view");
-		list_view.Init(146.0, 59.0, 179.0, 384.0);
-		list_view.getListView().setItems(CListView.items);
-		list_view.getListView().getSelectionModel().selectedItemProperty().addListener(
-				(ObservableValue<? extends String> ov, String old_val,
-						String new_val) -> {
-		       	 Controller.displaySelectedItem(list_view.getListView(), web_view.getWebView());
-		});
-		// TextFilde search
-		CTextField text_filde_search = new CTextField(primaryCScene.getRoot(),"text-filde-search");
-		text_filde_search.Init(332.0, 59.0, 267.0, 31.0);
-		text_filde_search.getTextField().setPromptText("Search word here...");
-		text_filde_search.getTextField().setOnKeyReleased(new EventHandler<InputEvent>() {  
-            @Override  
-            public void handle(InputEvent arg0) {  
-                // TODO Auto-generated method stub  
-            	list_view.showListView(text_filde_search.getTextField().getText());
-            }  
-        }); 
+			// web view
+			web_view.init(332.0, 95.0, 388.0, 347.0);
+			// ListView
+			list_view.Init(146.0, 59.0, 179.0, 384.0);
+			list_view.getListView().setItems(CListView.items);
+			list_view.getListView().getSelectionModel().selectedItemProperty().addListener(
+					(ObservableValue<? extends String> ov, String old_val,
+							String new_val) -> {
+			       	 Controller.displaySelectedItem(list_view.getListView(), web_view.getWebView(),new_val);
+			});
+			// TextFilde search
+			text_filde_search.Init(332.0, 17.0, 267.0, 31.0);
+			text_filde_search.getTextField().setPromptText("Search word here...");
+			text_filde_search.getTextField().setOnKeyReleased(new EventHandler<InputEvent>() {  
+	            @Override  
+	            public void handle(InputEvent arg0) {  
+	                // TODO Auto-generated method stub  
+	            	list_view.showListView(text_filde_search.getTextField().getText());
+	            }  
+	        }); 
 		
 		return primaryCScene.getScene();
 	}
