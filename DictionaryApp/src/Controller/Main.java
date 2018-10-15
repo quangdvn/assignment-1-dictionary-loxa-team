99d5dc3bdc19b9ed4fb1e5a8b8c1ec458cc146e9
quangnhat99 <ghitacluber@gmail.com>
2018-10-15 22:45:21 +0700

    config smt

--- DictionaryApp/src/Controller/Main.java
+++ DictionaryApp/src/Controller/Main.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,11 +12,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../FXML_Design/MainDictionary.fxml"));
+       Parent root = FXMLLoader.load(getClass().getResource("../FXML_Design/MainDictionary.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().add(getClass().getResource("../CSS/Style.css").toExternalForm());
         primaryStage.setTitle("Dictionary");
         primaryStage.getIcons().add(new Image("/Image/Dictionary.png"));
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+        primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setOnCloseRequest(e -> Platform.exit());
     }
 
 
