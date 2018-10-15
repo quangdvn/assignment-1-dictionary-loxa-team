351fee1d46de0313fa57b9dbbfd15369e38bcdb9
NgocHai220998 <41608661+NgocHai220998@users.noreply.github.com>
2018-10-15 20:52:09 +0700

    3nd commit

--- AppDictionaryAnother/src/MyFX/AlertBoxAdd.java
+++ AppDictionaryAnother/src/MyFX/AlertBoxAdd.java
@@ -10,12 +10,13 @@ import javafx.stage.Stage;
 
 public class AlertBoxAdd {
 	public static void display(String title) {
-
+		
 		Stage window = new Stage();
 		window.initModality(Modality.APPLICATION_MODAL);
 		
 		AnchorPane root = new AnchorPane();
-		root.setPrefSize(599.0, 214.0);
+		root.setPrefSize(599.0, 250.0);
+		Scene scene = new Scene(root,599,250);
 		
 		CTextArea target = new CTextArea(root,"");
 		target.Init(14.0, 14.0, 252.0, 184.0);
@@ -24,8 +25,21 @@ public class AlertBoxAdd {
 		explain.getTextArea().setPromptText("Word explain here...");
 		explain.Init(333.0, 14.0, 252.0, 184.0);
 		
-		CButton button = new CButton(root,"");
+		CButton button = new CButton(root,"button");
 		button.Init(267.0, 73.0, 65.0, 68.0, "Okie");
+		button.setImage("google.png", 30, 40);
+		button.setCSS(scene);
+		
+		CButton exit = new CButton(root,"button-quit");
+		exit.setImage("quit.png", 30, 40);
+		exit.Init(14.0, 200.0, 570.0, 40.0, "Exit");
+		exit.setCSS(scene);
+		
+		
+		
+		exit.getButton().setOnAction(e -> window.close());
+		
+		
 		button.getButton().setOnAction(new EventHandler<ActionEvent>() {
 		       @Override
 		       public void handle(ActionEvent event) {
@@ -34,7 +48,6 @@ public class AlertBoxAdd {
 		       }
 		});
 		
-		Scene scene = new Scene(root,599,214);
 		
 		window.setTitle(title);
 		window.setScene(scene);
